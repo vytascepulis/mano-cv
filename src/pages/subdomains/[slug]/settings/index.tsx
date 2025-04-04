aba779888ccf940247a93bacf2b1a1609d5f65bd
Vytautas <cepulis.vytas@gmail.com>
2025-04-04 03:05:37 +0300

    commit

--- src/pages/subdomains/[slug]/settings/index.tsx
+++ src/pages/subdomains/[slug]/settings/index.tsx
@@ -1,10 +1,7 @@
-import NotFoundPage from "@/pages/404";
-import { useAuth } from "@/contexts/AuthContext";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
 const SettingsPage = () => {
-  const { token } = useAuth();
   const { data: session } = useSession();
   const router = useRouter();
   const slug = router.query.slug;
