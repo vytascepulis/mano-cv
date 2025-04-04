13f699add7537a010aa86483c5e36b82e0bea3fc
Vytautas <cepulis.vytas@gmail.com>
2025-04-04 03:18:19 +0300

    update protocol depending on env

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -26,6 +26,7 @@ const useFetch = <T>({ endpoint, method = "GET" }: Props) => {
     setIsLoading(true);
 
     const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+    console.log(process.env);
 
     try {
       const response = await axios({
