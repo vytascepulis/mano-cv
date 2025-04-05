7e686f6b4804f21f9f253a9edd3e3ffd691f2eee
Vytautas <cepulis.vytas@gmail.com>
2025-04-05 11:52:17 +0300

    test wildcard cors

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -2,20 +2,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handleCors = (req: NextApiRequest, res: NextApiResponse) => {
   const origin = req.headers.origin;
-  const allowedDomainPattern =
-    /^https:\/\/([a-z0-9-]+\.)?mano-cv\.lt$|^http:\/\/localhost(:\d+)?$/;
+  // const allowedDomainPattern =
+  //   /^https:\/\/([a-z0-9-]+\.)?mano-cv\.lt$|^http:\/\/localhost(:\d+)?$/;
   console.log("handleCors", origin);
 
-  if (origin && allowedDomainPattern.test(origin)) {
-    console.log("set cors res");
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization",
-    );
-  }
+  // if (origin && allowedDomainPattern.test(origin)) {
+  console.log("set cors res");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  // }
 
   if (req.method === "OPTIONS") {
     res.status(200).end(); // End preflight request
