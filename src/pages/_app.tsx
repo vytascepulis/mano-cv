152caddcb6e0ce2e89fe263e6a10d74d58c55335
Vytautas <cepulis.vytas@gmail.com>
2025-04-05 11:29:25 +0300

    disable session refetch on window focus

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={session} refetchOnWindowFocus={false}>
       <Component {...pageProps} />
     </SessionProvider>
   );
--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -4,8 +4,10 @@ const handleCors = (req: NextApiRequest, res: NextApiResponse) => {
   const origin = req.headers.origin;
   const allowedDomainPattern =
     /^(https?:\/\/)?([a-z0-9-]+\.)?(localhost(:\d+)?|mano-cv\.lt)$/;
+  console.log("handleCors", origin);
 
   if (origin && allowedDomainPattern.test(origin)) {
+    console.log("set cors res");
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
