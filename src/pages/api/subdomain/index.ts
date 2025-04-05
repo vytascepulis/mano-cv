922ecd44b4ad7f807012411c3fef89f6f4363a31
Vytautas <cepulis.vytas@gmail.com>
2025-04-05 11:33:39 +0300

    fix

--- src/pages/api/subdomain/index.ts
+++ src/pages/api/subdomain/index.ts
@@ -15,7 +15,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>,
 ) {
+  console.log("call api route");
   if (handleCors(req, res)) return;
+  console.log("after handleCors");
 
   const method = req.method;
   const subdomain = getSubdomainFromUrl(req.headers.origin!);
