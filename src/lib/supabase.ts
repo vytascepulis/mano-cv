704868ca07260afe5889dcc2429b777721e973f8
Vytautas <cepulis.vytas@gmail.com>
2025-04-08 21:25:52 +0300

    added tailwind, shadcn

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types/supabase.types";
-import { sha256 } from "@/utils/crypto";
 
 export const supabase = createClient<Database>(
   process.env.SUPABASE_URL!,
