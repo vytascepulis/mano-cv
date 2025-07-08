f1226ee3f72e006d962839d358f203509a6ad969
Vytautas <cepulis.vytas@gmail.com>
2025-07-08 13:04:37 +0300

    update max width on setting description

--- src/pages/subdomains/[slug]/profilis/Setting.tsx
+++ src/pages/subdomains/[slug]/profilis/Setting.tsx
@@ -18,7 +18,7 @@ const Setting = ({ title, subtitle, optional, separated, children }: Props) => {
         )}
       </h2>
       <div className="flex flex-col items-start justify-between lg:flex-row">
-        <p className="mb-3 shrink-0 text-[17px] font-light lg:max-w-[250px]">
+        <p className="mb-3 shrink-0 text-[17px] font-light lg:max-w-[300px]">
           {subtitle}
         </p>
         <div className="flex w-full max-w-[400px] grow lg:justify-end">
